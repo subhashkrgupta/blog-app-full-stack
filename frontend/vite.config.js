ab07b75aae3fcdceabdf05b175bad48a6f6e5f31
Subhash Kumar <130248657+subhashkrgupta@users.noreply.github.com>
2026-02-17 14:40:46 +0530

    add server proxy for the connection frontend and backend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
+  plugins: [
+    react(),
     tailwindcss()
   ],
-     
-})
+  server: {
+    proxy: {
+      "/api/v1": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
